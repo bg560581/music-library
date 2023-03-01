ec3deeb16738d0f1b2b2fa89bc099a76ab561e1f
bg560581 <bennett.galgano@gmail.com>
2023-03-01 15:11:38 -0500

    expand applications with react router dom

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import { useContext, useState } from 'react'
+import { SearchContext } from '../context/SearchContext'
+
+function SearchBar() {
+  const {term, handleSearch} = useContext(SearchContext)
+
+  return (
+    <form>
+        <input ref={term} type='text' placeholder='Search Here' />
+        <button onClick={(e) => handleSearch(e, term.current.Value)}>Submit</button>
+    </form>
+  )
+}
+
+export default SearchBar
